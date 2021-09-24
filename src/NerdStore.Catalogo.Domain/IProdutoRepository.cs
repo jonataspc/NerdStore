8facf0e5a0c0a9fb64f20d1434d64ceae4daa241
Jonatas Cruz <jonataspc@gmail.com>
2021-09-24 14:18:38 -0300

    feat: adding data layer and repositories classes

---
+++ src/NerdStore.Catalogo.Domain/IProdutoRepository.cs
@@ -0,0 +1,26 @@
+﻿using NerdStore.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<IEnumerable<Produto>> ObterTodos();
+
+        Task<Produto> ObterPorId(Guid id);
+
+        Task<IEnumerable<Produto>> ObterPorCategoria(int codigo);
+
+        Task<IEnumerable<Categoria>> ObterCategorias();
+
+        void Adicionar(Produto produto);
+
+        void Adicionar(Categoria categoria);
+
+        void Atualizar(Produto produto);
+
+        void Atualizar(Categoria categoria);
+    }
+}
