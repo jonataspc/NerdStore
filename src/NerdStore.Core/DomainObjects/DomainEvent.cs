3e887472545dd728a062934edb6ace144af37473
Jonatas Cruz <jonataspc@gmail.com>
2021-10-13 20:40:07 -0300

    feat: adding application services.

---
+++ src/NerdStore.Core/DomainObjects/DomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using NerdStore.Core.Messages;
+using System;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public abstract class DomainEvent : Event
+    {
+        protected DomainEvent(Guid aggregateId)
+        {
+            AggregateId = aggregateId;
+        }
+    }
+}
